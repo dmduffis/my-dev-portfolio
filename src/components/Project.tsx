34b1272d4d5cb067605ea79d25f29babda8381a5
Daniel Matthew Duffis <dmduffis@gmail.com>
2024-05-03 19:15:56 -0700

    added project template

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -9,4 +9,14 @@ function Project() {
 
         <div className="project-info">
         <p className='project-title'>Jaqueline Lima</p>
-        <p className='tags'>A respo
+        <p className='tags'>A responsive, portfolio website for upcoming Brazilian architect.</p>
+        <p className='btn-project'>View Live Demo</p>
+
+        </div>
+
+    </div>
+    
+  )
+}
+
+export default Project
