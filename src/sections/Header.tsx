1a882db33c8e4d00cbc05457a37dd491f3b1fa26
Daniel Matthew Duffis <dmduffis@gmail.com>
2024-05-24 19:08:07 -0400

    layout adjustments

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className='text-gray-100'>
         <div>
-               <h1 className='m-2 text-center'>Coding with Conscience</h1>
+               <h1 className='mt-48 text-center'>Coding with Conscience</h1>
                <h2 className='m-2 text-center'>I craft web and mobile experiences for good</h2>
         </div>
     </header>
--- src/sections/Navigation.tsx
+++ src/sections/Navigation.tsx
@@ -1,15 +1,19 @@
 import './navigation.style.css'
+import logo from '../assets/images/lgo_square_white.png'
 
 function Navigation() {
   return (
-    <nav className='text-gray-100'>
-        <div className='site-title'>Daniel Duffis</div>
-        <div className='navigation-links'>
-            <ul>
-                <li>About</li>
-                <li>Contact</li>
-                <li>Resume</li>
-            </ul>
+    <nav className='flex justify-center top-0 left-0 right-0 gap-20 bg-gray-950 text-gray-100 border-b border-gray-800 fixed backdrop-blur-3xl opacity-95'>
+        <div id="max" className='flex justify-between w-full px-10'>
+              <div className='site-title'>Daniel Duffis</div>
+              <div className='navigation-links'>
+              
+                  <ul>
+                      <li>About</li>
+                      <li>Contact</li>
+                      <li>Resume</li>
+                  </ul>
+              </div>
         </div>
     </nav>
   )
