d5db1fa16d9127ba8acd4a4e25e756d301326844
Daniel Matthew Duffis <dmduffis@gmail.com>
2024-05-27 13:00:33 -0400

    responsive nav

--- src/sections/Navigation.tsx
+++ src/sections/Navigation.tsx
@@ -3,16 +3,18 @@ import './navigation.style.css'
 function Navigation() {
   return (
     <nav className='flex justify-center top-0 left-0 right-0 gap-20 bg-gray-950 text-gray-100 border-b border-gray-800 fixed backdrop-blur-3xl opacity-95'>
-        <div id="max" className='flex justify-between w-full px-10'>
+        <div id="max" className='flex justify-between items-center w-full px-10'>
               <div className='site-title'>Daniel Duffis</div>
               <div className='navigation-links max-md:hidden'>
-              
                   <ul>
                       <li>About</li>
                       <li>Contact</li>
                       <li>Resume</li>
                   </ul>
               </div>
+              <div className='md:hidden'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill='white' viewBox="0 0 50 50" width="24px" height="24px"><path d="M 3 9 A 1.0001 1.0001 0 1 0 3 11 L 47 11 A 1.0001 1.0001 0 1 0 47 9 L 3 9 z M 3 24 A 1.0001 1.0001 0 1 0 3 26 L 47 26 A 1.0001 1.0001 0 1 0 47 24 L 3 24 z M 3 39 A 1.0001 1.0001 0 1 0 3 41 L 47 41 A 1.0001 1.0001 0 1 0 47 39 L 3 39 z"/></svg>
+                </div>
         </div>
     </nav>
   )
