d9020214ac81e80a9dfaa3c9c8150a362d16396d
Daniel Matthew Duffis <dmduffis@gmail.com>
2024-05-27 12:31:51 -0400

    responsive projects

--- src/sections/Navigation.tsx
+++ src/sections/Navigation.tsx
@@ -5,7 +5,7 @@ function Navigation() {
     <nav className='flex justify-center top-0 left-0 right-0 gap-20 bg-gray-950 text-gray-100 border-b border-gray-800 fixed backdrop-blur-3xl opacity-95'>
         <div id="max" className='flex justify-between w-full px-10'>
               <div className='site-title'>Daniel Duffis</div>
-              <div className='navigation-links'>
+              <div className='navigation-links max-md:hidden'>
               
                   <ul>
                       <li>About</li>
