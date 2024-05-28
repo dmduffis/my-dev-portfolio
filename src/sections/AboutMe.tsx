195a18fbd484eba752590fd096e3a92c42ff3ad8
Daniel Matthew Duffis <dmduffis@gmail.com>
2024-05-28 18:34:35 -0400

    skill section

--- src/sections/AboutMe.tsx
+++ src/sections/AboutMe.tsx
@@ -1,14 +1,42 @@
 import './aboutme.style.css'
+import tailwind from '../assets/images/tailwind-logo.png';
+import typescript from '../assets/images/typescript-plain-icon-256x256-ypojgpyj.png';
+import reactnative from '../assets/images/react_native_logo.png'
+import nextjs from '../assets/images/next-js-icon.png'
+import nodejs from '../assets/images/node_icon.png'
+import mongodb from '../assets/images/mongodb_icon.png'
+import firebase from '../assets/images/firebase_logo.png'
+import hygraph from '../assets/images/hygraph-icon.png'
 
 function AboutMe() {
   return (
-        <div className='rounded-3xl p-2 mt-24 border border-gray-700'>
+        <div className='rounded-3xl mt-10'>
         <h1></h1>
-          <div className='section-wrapper'>
-          <h1 className='section-title mx-10 mt-16  text-left text-white'>Front-End Developer</h1>
-          <h3 className='section-subtitle mx-10 mt-1  text-left text-gray-400'>Bringing over a decade of graphic design experience into the software engineering space</h3>
-            <div className="flex flex-wrap">
+          <div className='section-wrapper sm:w-full lg:w-7/12 '>
+        <div className='w-full p-10 bg-gray-900 rounded-3xl'>
+          <h1 className='section-title text-left text-white'>Jr. Front-End Developer</h1>
+          <h3 className='mt-1 text-lg text-left text-gray-400'>I bring over a decade of graphic design experience and expertise into the realm of software engineering. My proficiency lies in front-end development, complemented by a foundational understanding of back-end technologies.</h3>
+        
+        {/* List of Skills */}
+        <div className="flex flex-wrap w-full max-sm:justify-between sm:gap-24 max-md:justify-left md:gap-24 lg:justify-left lg:gap-20">
             <div>
+                <h2 className='skills-title mt-8 mb-2  text-left text-white'>Front-End</h2>
+                <ul className='text-lg text-gray-200 space-y-4'>
+                    <li className='flex gap-4 align-center'><img src={tailwind} width={28} height={28} />Tailwind</li>
+                    <li className='flex gap-4 align-center'><img src={typescript} width={28} height={28} />TypeScript</li>
+                    <li className='flex gap-4 align-center'><img src={reactnative} width={28} height={28} />ReactJS | Native</li>
+                    <li className='flex gap-4 align-center'><img src={nextjs} width={28} height={28} />Next.js</li>
+                </ul>
+            </div>
+            <div>
+                <h2 className='skills-title mt-8 mb-2 text-left text-white'>Back-End</h2>
+                <ul className='text-lg text-gray-200 space-y-4'>
+                    <li className='flex gap-4 align-center'><img src={nodejs} width={28} height={28} />Node.js | Express.js</li>
+                    <li className='flex gap-4 align-center'><img src={mongodb} width={28} height={28} />MongoDB (noSQL)</li>
+                    <li className='flex gap-4 align-center'><img src={firebase} width={28} height={28} />FireBase (noSQL) </li>
+                    <li className='flex gap-4 align-center'><img src={hygraph} width={28} height={28} />Hygraph (GraphQL)</li>
+                </ul>
+            </div>
             </div>
             </div>
           </div>
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -4,8 +4,8 @@ function Header() {
   return (
     <header className='text-gray-100'>
         <div>
-               <h1 className='mt-48 text-center'>Coding with Conscience</h1>
-               <h2 className='m-2 text-center'>I craft web and mobile experiences for good</h2>
+               <h1 className='mt-48 text-center'>Hi, I'm Daniel</h1>
+               <h2 className='m-2 text-center'>I develop and deliver beautiful web and mobile apps</h2>
         </div>
     </header>
   )
